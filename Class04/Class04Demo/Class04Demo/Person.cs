dd2f8f9d871a08ae4c9f57a3333f6c9fc0e213d6
Iverson, Amanda <amanda@codefellows.com>
2018-03-22 22:05:36 -0700

    Add Class Demo

---
+++ Class04/Class04Demo/Class04Demo/Person.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class04Demo
+{
+    public class Person
+    {
+        public Person(string name, int ages)
+        {
+            Name = name;
+            age = ages;
+        }
+        //public Person()
+        //{
+
+        //}
+
+
+        public int Height { get; set; }
+        public string EyeColor { get; set; }
+        public string HairColor { get; set; }
+
+        private string name; // backing store
+        private int age;
+        //  public string Name { get; set; }
+
+        public string FavoriteColor { get; private set; } = "Red";
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+           set
+            {
+                if (age < 21)
+                {
+                    Console.WriteLine($"{Name} is not old enough");
+                }
+                else
+                {
+                    age = value;
+                }
+                //age = value;
+            }
+        }
+
+        private int number;
+
+        public int Number
+        {
+            get
+            {
+                return number++;
+            }
+
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string SayHello()
+        {
+            return "Hello!!";
+        }
+
+        public string SayGoodbye()
+        {
+            return "Goodbye";
+
+        }
+
+        public string Dance()
+        {
+            return "Chicken Dance";
+        }
+
+
+
+
+    }
+}
---
+++ Class04/Class04Demo/Class04Demo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Class04Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Person amanda = new Person("Amanda", 50); // amanda is my object of the Person Class
+            //Console.WriteLine(amanda.SayHello());
+
+            //Person miya = new Person("Miya", 40);
+
+            //miya.Dance();
+
+            //amanda.Name = "Amanda"; // set 
+            //string name = amanda.Name; // get;
+
+            Person greg = new Person("Greg", 45) { Name = "Scott", Age = 28, EyeColor = "purple", HairColor = "yellow" };
+
+            Console.WriteLine(greg.Name);
+            Console.WriteLine(greg.Age);
+
+            //miya.Name = "Miya";
+            //miya.Age = 40;
+            //miya.Height = 56;
+            //miya.HairColor = "pink";
+
+
+            Board game = new Board();
+
+            Player player1 = new Player("Amanda", "X");
+            Player player2 = new Player("Miya", "@");
+            player1.MakeMark(game);
+
+
+        }
+
+        static void MyMethod()
+        {
+            Console.WriteLine("Hello!!!");
+        }
+    }
+}
