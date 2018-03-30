5f51d17e0f76b8243878da71d92f3fdc7b58346f
Iverson, Amanda <amanda@codefellows.com>
2018-03-30 16:01:22 -0700

    add Stack Example

---
+++ Data_Structures/Implementations/StackExample/StackExample/Stack.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackExample
+{
+   public class Stack
+    {
+        public Node Head { get; set; }
+        public Stack(Node node)
+        {
+            Head = node;
+        }
+
+        public void Push(Node node)
+        {
+            node.Next = Head;
+            Head = node;
+        }
+
+        public Node Peek() => Head;
+    }
+}
