13aa090ccdc9cda50b98ceef122d6bfa0cff9e07
Iverson, Amanda <amanda@codefellows.com>
2018-04-02 21:24:02 -0700

    Add Class 11 Demo

---
+++ Class11/Class11Demo/Class11Demo/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Class11Demo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Class11Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string name, string color)
+        {
+            //List<string> myList = new List<string>() { "German Shepard", "Great Dane", "Border Collie", "Beagle", "HotDog" };
+
+            return RedirectToAction("Students", new { name, color });
+
+            // return View(myList);
+        }
+
+        public ViewResult Students(string name, string color)
+        {
+            //Student students = new Student();
+
+            Student students = Student.GetStudent(name, "Dotnet", 4.1, color);
+            Student students1 = Student.GetStudent("Brent", "Crazy", 0.7, "blue");
+            Student students2= Student.GetStudent("Peter", "Dance", 3.5, "pink");
+
+
+
+            List<Student> myStudents = new List<Student> { students, students1, students2 };
+            return View(myStudents);
+        }
+    }
+}
---
+++ Class11/Class11Demo/Class11Demo/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Class11Demo.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public double GPA { get; set; }
+
+        public string Color { get; set; }
+
+        public static Student GetStudent(string name, string cl, double gpa, string color )
+        {
+            Student student = new Student() { Name = name, Class = cl, GPA = gpa, Color = color };
+            return student;
+        }
+    }
+}
