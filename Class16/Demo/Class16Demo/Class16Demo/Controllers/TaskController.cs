4f1a3bc8ddb35fbcc7d6474d9c7f02775c31370e
Iverson, Amanda <amanda@codefellows.com>
2018-04-09 17:06:51 -0700

    Add Class 16 Demo and Class Drawing

---
+++ Class16/Demo/Class16Demo/Class16Demo/Controllers/TaskController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Class16Demo.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Class16Demo.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Task")]
+    public class TaskController : Controller
+    {
+        private TaskDbContext _context;
+
+        public TaskController(TaskDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] {"value1", "value2", "cat"};
+        }
+
+        [HttpGet("{id:int?}")]
+        public IActionResult Get(int id)
+        {
+           var myTask = _context.Tasks.FirstOrDefault(t => t.ID == id);
+            return Ok(myTask);
+        }
+    }
+}
---
+++ Class16/Demo/Class16Demo/Class16Demo/Data/TaskDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Class16Demo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Class16Demo.Data
+{
+    public class TaskDbContext : DbContext
+    {
+        public TaskDbContext(DbContextOptions<TaskDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Tasks> Tasks { get; set; }
+    }
+}
